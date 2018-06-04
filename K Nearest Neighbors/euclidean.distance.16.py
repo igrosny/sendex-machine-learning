971c58ade5c83d9ce0986cffa1e9943b47b41b84
igrosny <igrosny@gmail.com>
2018-06-04 19:34:19 -0300

    Distancia euclide parte 16

---
+++ K Nearest Neighbors/euclidean.distance.16.py
@@ -0,0 +1,33 @@
+import numpy as np 
+from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import style
+from collections import Counter
+
+# Es el estilo con el que quiero plotear
+style.use('fivethirtyeight')
+
+# este es un diccionario con el dos grupos de datos
+# Esta dividido asi para poder usar la primer parte como colores
+dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
+
+# este es el nuevo que quiero saber a donde pertenece
+new_features = [5,7]
+
+# itero y ploteo todo el diccionario
+for i in dataset:
+    for ii in dataset[i]:
+        plt.scatter(ii[0],ii[1], s=100, color=i)
+
+# ploteo el nueo feature
+plt.scatter(new_features[0],new_features[1], s=100, color='b')
+
+# muestro el plot
+plt.show()
+
+# def k_nearest_neighbors(data, predict, k=3):
+#     if len(data) >= k:
+#         warnings.warn("K is set to a value")
+
+    
