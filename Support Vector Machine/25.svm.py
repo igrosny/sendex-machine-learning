3bbb4c94f8005c2f66dc75179df9afcb44b36b87
igrosny <igrosny@gmail.com>
2018-06-06 20:01:29 -0300

    Clase 27, termino el SVM

--- Support Vector Machine/25.svm.py
+++ Support Vector Machine/25.svm.py
@@ -57,7 +57,40 @@ class Support_Vector_Machine:
             # cuando converge
             optimized = False
             while not optimized:
-                pass
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                        self.max_feature_value * b_range_multiple,
+                        step * b_multiple):
+                    for transformation in transforms:
+                        w_t = w*transformation
+                        found_option = True
+                        # Es eslavon mas fragil del SVM
+                        # SMO - ver que es
+                        # yi(xi.w + b) >= 1
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t,xi)+b) >= 1:
+                                    found_option = False
+                                    break
+
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+
+                if w[0] < 0:
+                    optimized = True 
+                    print('Optimized a step')
+                else:
+                    # w
+                    w = w - step
+
+            # todas las distancias
+            norms = sorted([n for n in opt_dict])
+            opt_choice = opt_dict[norms[0]]
+
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+
+            latest_optimum = opt_choice[0][0]+step * 2
 
         
 
