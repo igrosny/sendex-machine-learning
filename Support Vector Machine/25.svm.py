4d15da01fb1c0b122a04d66e2f924c6c6fed4aac
igrosny <igrosny@gmail.com>
2018-06-05 22:02:32 -0300

    Clase numeo 25 - Creando un SVN de cero

---
+++ Support Vector Machine/25.svm.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+style.use('ggplot')
+
+class Support_Vector_Machine:
+    def __init__(self, visualization=True):
+        self.visualization = visualization
+        self.colors = {1:'r', -1:'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
+
+    # Esta es la funcion para entrenar
+    def fit(self, data):
+        pass
+
+    # Esta es la funcion para predecir
+    def predict(self, features):
+        # sign (x.w+b)
+        classification = np.sign(np.dot(np.array(features), self.w)+self.b)
+        return classification
+
+
+
+# Creamos un diccionario
+data_dict = {-1:np.array([[1,6],
+                        [2,8],
+                        [3,8],]),
+            1:np.array([[5,1],
+                        [6, -1],
+                        [7,3],])}
