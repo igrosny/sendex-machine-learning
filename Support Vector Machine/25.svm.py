4425eb98d299a0de88847c803292bff1bd474cf5
igrosny <igrosny@gmail.com>
2018-06-07 21:34:27 -0300

    comentarios en el codigo

--- Support Vector Machine/25.svm.py
+++ Support Vector Machine/25.svm.py
@@ -20,6 +20,8 @@ class Support_Vector_Machine:
         # data es un diccionario con dos clases -1 y 1
         self.data = data
         #{ ||w||: [w,b]}
+        # Este diccionario tiene como clave la distancia
+        # y una lista con los valores w y b
         opt_dict = {}
 
         transforms = [[1,1],
@@ -83,44 +85,53 @@ class Support_Vector_Machine:
                         for i in self.data:
                             #itero las filas
                             for xi in self.data[i]:
-                                yi = i
+                                yi = i # si la clase es negative multiplica por -1
+                                # Si i es positivo mutlplica por 1
                                 if not yi * (np.dot(w_t , xi) + b) >= 1:
                                     found_option = False
                                     break
 
                         if found_option:
+                            # Calculo la norm del vector
+                            # que la norm es la longitud dle vector
                             opt_dict[np.linalg.norm(w_t)] = [w_t, b]
 
                 if w[0] < 0:
                     optimized = True 
                     print('Optimized a step')
                 else:
-                    # w
+                    # Le resto un paso
                     w = w - step
 
-            # todas las distancias
+            # todas las longitudes ordenadas de menors a mayo
             norms = sorted([n for n in opt_dict])
+            # selecciono los valores del optdi
             opt_choice = opt_dict[norms[0]]
 
             self.w = opt_choice[0]
             self.b = opt_choice[1]
 
+            print(opt_choice)
             latest_optimum = opt_choice[0][0] + step * 2
 
         
 
-    # Esta es la funcion para
+    # Esta es la funcion para predecir
     def predict(self, features):
         # sign (x.w+b)
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+
+        # Dibuja el punto a predecir
         if classification !=0 and self.visualization:
             self.ax.scatter(features[0], features[1], s=200, marker='*',c=self.colors[classification])
         
         return classification
 
     def visualize(self):
+        # Pone un punto por cada valor del dict
         [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
 
+        # Crea la funcion para calcular el hiperplano
         def hyperplane(x,w,b,v):
             return (-w[0]*x-b+v) / w[1]
 
